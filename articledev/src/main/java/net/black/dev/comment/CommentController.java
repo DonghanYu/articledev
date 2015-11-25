d796a90ff4714620393f62b0de8d9c22c3197c03
DonghanYu <realtiger0982>
2015-11-25 16:04:40 +0900

    a

--- articledev/src/main/java/net/black/dev/comment/CommentController.java
+++ articledev/src/main/java/net/black/dev/comment/CommentController.java
@@ -1,4 +1,37 @@
 package net.black.dev.comment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/board/{bno}/{pg}/{no}")
 public class CommentController {
+	private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
+
+	@Autowired
+	private CommentService commentService;
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public void view(@PathVariable long no) {
+		logger.info(no + "번 댓글 상세보기");
+		
+	}
+	@RequestMapping(value="/update", method=RequestMethod.GET)
+	public void update(@PathVariable long no) {
+		logger.info(no + "번 댓글 수정 폼 출력");
+	}
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public void view(@PathVariable long no, @ModelAttribute CommentVO commentVO) {
+		logger.info(no + "번 댓글 수정 처리");
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.GET)
+	public void delete(@PathVariable long no) {
+		logger.info(no + "번 댓글 삭제 처리");
+	}
 }
--- articledev/src/main/java/net/black/dev/comment/CommentServiceImpl.java
+++ articledev/src/main/java/net/black/dev/comment/CommentServiceImpl.java
@@ -1,5 +1,35 @@
 package net.black.dev.comment;
 
-public class CommentServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+public class CommentServiceImpl implements CommentService {
+
+	@Autowired
+	@Qualifier("CommentDAO")
+	private CommentDAO commentDAO;
+	
+	
+	@Override
+	public void insertComment(CommentVO commentVO) throws Exception {
+		commentDAO.insertComment(commentVO);
+		
+	}
+	
+	@Override
+	public String getCommentList(long no) throws Exception {
+		return commentDAO.getCommentList(no);
+	}
+	
+	@Override
+	public void updateComment(CommentVO commentVO) throws Exception {
+		commentDAO.updateComment(commentVO);
+		
+	}
+
+	@Override
+	public void deleteComment(CommentVO commentVO) throws Exception {
+		commentDAO.deleteComment(commentVO);
+		
+	}
 }
